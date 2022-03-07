fe7ef96f7084fd971749d11cc76b49dc80a4f96b
Facundo <facundogds@gmail.com>
2022-03-07 01:56:25 +0000

    Add 78/79

--- exercises/078-convertDoubleSpaceToSingle/app.js
+++ exercises/078-convertDoubleSpaceToSingle/app.js
@@ -1,5 +1,19 @@
 function convertDoubleSpaceToSingle(str) {
-    // your code here
+    let newarray = str.split("  ");//separador de 2 spaces
+    return newarray.join(" ");//
 }
+
+
 var output = convertDoubleSpaceToSingle("string  with  double  spaces");
 console.log(output); // --> "string with double spaces"
+
+/* Split convierte un string en un array separado
+por el <separador> que elijas, siempre entrecomillado
+de la siguiente manera: " ". Así separará el string
+a partir de elementos que estén separados
+por una barra de espacio " ", también valen comas,
+barras de guion... Todo eso devuelto en un array*/
+
+/* Y el método Join permite devolver a string
+un array cuyos elementos se juntan según como sea
+indicado: si separado con comas, o pegado en barras */
--- exercises/079-joinThreeArrays/app.js
+++ exercises/079-joinThreeArrays/app.js
@@ -1,3 +1,28 @@
 function joinThreeArrays(arr1, arr2, arr3) {
-  // your code here
+  let newarray = arr1.concat(arr2);
+  let newarray2 = newarray.concat(arr3);
+  return newarray2;
 }
+//Es más eficiente usar el método concat().
+
+var output = joinThreeArrays([1, 2], [3, 4], [5, 6]);
+console.log(output); // --> [1, 2, 3, 4, 5, 6]
+
+
+/* Esta es una forma válida pero ineficiente.
+
+function joinThreeArrays(arr1, arr2, arr3) {
+  let newarray = [];
+  for (i1=0;i1<arr1.length;i1++){
+    newarray.push(arr1[i1]);
+  }
+  for (i2=0;i2<arr2.length;i2++){
+    newarray.push(arr2[i2]);
+  }
+  for (i3=0;i3<arr3.length;i3++){
+    newarray.push(arr3[i3]);
+  }
+  return newarray;
+}
+
+*/
